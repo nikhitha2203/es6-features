986c0fdfc98727aaaeb811ce8f50359e1d5fe584
Nikhitha <nikhithautada2003@gmail.com>
2024-08-06 17:29:47 +0530

    async done

--- es6-features/script.js
+++ es6-features/script.js
@@ -21,3 +21,18 @@ const data = {
   console.log(country)
 
   //Task-2: 
+
+const [a, b] = [1];
+console.log(a * b); // <== ???const [a, b = 1] = [2];
+console.log(a * b); // <== ???let [a, b = 2, c, d = 1] = [3, 4];
+console.log(a, b, c, d); // error
+
+//TASK 3 : Let's create a counter that will print a number in a sequence each second.
+let count = 0;
+function repeat() {
+    console.log(count);
+    count += 1;
+    setTimeout(repeat, 1000);
+}
+setTimeout(repeat, 1000);
+
